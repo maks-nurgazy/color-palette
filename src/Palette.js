29eb645334e7af29f2196dc37d7e62ddbd56e35b
maks-nurgazy <maksnurgazy@gmail.com>
2020-08-17 19:14:50 +0600

    added palette

---
+++ src/Palette.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+
+class Palette extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {}
+    }
+    render() {
+        return (
+            <div className="Palette">
+                {/* Navbar goes here */}
+                <div className="Palette-colors">
+                    {/* Bunch of color boxes */}
+                </div>
+                {/* footer eventually */}
+            </div>
+        );
+    }
+}
+
+export default Palette;
