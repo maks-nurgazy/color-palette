c24640162a8f919d541f194a5ce7374c61b46bd7
maks-nurgazy <maksnurgazy@gmail.com>
2020-10-20 16:08:56 +0600

    Readme changed

--- src/App.js
+++ src/App.js
@@ -1,41 +1,44 @@
-import React, { Component } from 'react';
-import Palette from './Palette'
-import PaletteList from './PaletteList'
-import { Route, Switch } from 'react-router-dom'
-import seedColors from './seedColors'
-import { generatePalette } from './colorHelpers'
+import React, { Component } from "react";
+import { Route, Switch } from "react-router-dom";
+import Palette from "./Palette";
+import PaletteList from "./PaletteList";
+import SingleColorPalette from "./SingleColorPalette";
+import seedColors from "./seedColors";
+import { generatePalette } from "./colorHelpers";
 
 class App extends Component {
-
-  constructor(props) {
-    super(props)
-    this.state = {
-
-    }
-    this.findPalette = this.findPalette.bind(this)
-  }
-
   findPalette(id) {
-    return seedColors.find(palette => {
-      return palette.id === id
-    })
+    return seedColors.find(function(palette) {
+      return palette.id === id;
+    });
   }
-
   render() {
     return (
       <Switch>
         <Route
-          exact path='/'
-          render={() => <PaletteList palettes={seedColors} />} />
+          exact
+          path='/palette/:paletteId/:colorId'
+          render={() => <SingleColorPalette />}
+        />
         <Route
-          exact path='/palette/:id'
-          render={
-            (routeProps) => <Palette palette={generatePalette(this.findPalette(routeProps.match.params.id))} />}
+          exact
+          path='/'
+          render={routeProps => (
+            <PaletteList palettes={seedColors} {...routeProps} />
+          )}
+        />
+        <Route
+          exact
+          path='/palette/:id'
+          render={routeProps => (
+            <Palette
+              palette={generatePalette(
+                this.findPalette(routeProps.match.params.id)
+              )}
+            />
+          )}
         />
       </Switch>
-      // <div>
-      //   <Palette palette={generatePalette(seedColors[0])} />
-      // </div>
     );
   }
 }
